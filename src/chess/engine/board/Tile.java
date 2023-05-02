98962d6de17c801b8b243061e3a5436ba207e98e
aqueox <bartlomiejpecko@gmail.com>
2023-05-02 23:47:05 +0200

    changes to tile class, added guava.

---
+++ src/chess/engine/board/Tile.java
@@ -0,0 +1,66 @@
+package chess.engine.board;
+
+import chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Tile {
+    protected final int tileCoordinate;
+
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for (int i=0; i < 64; i++){
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        if (piece != null) {
+            return new Occupied(tileCoordinate, piece);
+        } else {
+            return EMPTY_TILES.get(tileCoordinate);
+        }
+    }
+    private Tile(int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+    public abstract boolean Occupied();
+
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile{
+        EmptyTile(final int coordinate) {
+            super(coordinate);
+        }
+
+        @Override
+        public boolean Occupied(){
+            return false;
+        }
+        @Override
+        public Piece getPiece(){
+            return null;
+        }
+    }
+    public static final class Occupied extends Tile{
+        private final Piece pieceOnTile;
+        Occupied(int tileCoordinate, Piece pieceOnTile){
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+        @Override
+        public boolean Occupied(){
+            return true;
+        }
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+}
