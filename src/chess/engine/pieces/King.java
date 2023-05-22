435164ad696223f70291b43abb320295b32893af
aqueox <bartlomiejpecko@gmail.com>
2023-05-23 00:26:40 +0200

    Added king move functionality

--- src/chess/engine/pieces/King.java
+++ src/chess/engine/pieces/King.java
@@ -2,17 +2,63 @@ package chess.engine.pieces;
 
 import chess.engine.Alliance;
 import chess.engine.board.Board;
+import chess.engine.board.BoardUtils;
 import chess.engine.board.Moves;
+import chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class King extends Piece{
+
+    private final static int[] CANDIDATE_MOVES = {-9, -8, -7, -1, 1, 7, 8, 9};
     King(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
     @Override
     public Collection<Moves> calcLegalMoves(Board board) {
-        return null;
+        final List<Moves> legalMoves = new ArrayList<>();
+
+        for(final int candidateOffset : CANDIDATE_MOVES){
+           final int destinationCoordinate = this.piecePosition + candidateOffset;
+
+           if(edges1(this.piecePosition, candidateOffset) || edges8(this.piecePosition, candidateOffset)){
+               continue;
+           }
+
+           if(BoardUtils.ValidCoordinate(destinationCoordinate)) {
+
+               final Tile destinationTile = board.getTile(destinationCoordinate);
+
+               if(!destinationTile.Occupied()) {
+                   legalMoves.add(new Moves.PieceMove(board, this, destinationCoordinate));
+               } else {
+
+                   final Piece pieceAtDestination = destinationTile.getPiece();
+                   final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+
+                   if (this.pieceAlliance != pieceAlliance) {
+
+                       legalMoves.add(new Moves.AttackMove(board, this, destinationCoordinate, pieceAtDestination));
+                   }
+               }
+           }
+        }
+
+
+
+        return ImmutableList.copyOf(legalMoves);
+    }
+    private static boolean edges1(final int currentPosition, final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == -1 ||
+                candidateOffset == 7) ;
+
+    }
+    private static boolean edges8(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.EIGHT_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 1 ||
+                candidateOffset == 9);
     }
 }
