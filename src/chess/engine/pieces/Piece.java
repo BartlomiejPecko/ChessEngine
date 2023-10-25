9ccaf363d77c97c40c2c8dd89ed8a8aa6dc16123
Bartłomiej Pecko <bartlomiejpecko@gmail.com>
2023-10-25 20:49:09 +0200

    hashcode, equals method for deleting previous move objects.

--- src/chess/engine/pieces/Piece.java
+++ src/chess/engine/pieces/Piece.java
@@ -5,6 +5,7 @@ import chess.engine.board.Board;
 import chess.engine.board.Moves;
 
 import java.util.Collection;
+import java.util.Objects;
 
 public abstract class Piece {
 
@@ -13,12 +14,37 @@ public abstract class Piece {
     protected final int piecePosition;
     protected final Alliance pieceAlliance; //black or white pieces
     protected final boolean isFirstPawnMove;
+    private final int cachedHashCode;
 
     Piece(final PieceType pieceType, final int piecePosition, final Alliance pieceAlliance) {
         this.pieceType = pieceType;
         this.piecePosition = piecePosition;
         this.pieceAlliance = pieceAlliance;
         this.isFirstPawnMove = false;
+        this.cachedHashCode = computeHashCode();
+    }
+    private int computeHashCode(){
+        int result = pieceType.hashCode();
+        result = 31 * result + pieceAlliance.hashCode();
+        result = 31 * result + piecePosition;
+        result = 31 * result + (isFirstPawnMove ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object other){
+        if(this == other){
+            return true;
+        }
+        if(!(other instanceof Piece)){
+            return false;
+        } final Piece otherPiece = (Piece) other;
+        return piecePosition == otherPiece.getPiecePosition() && pieceType == otherPiece.getPieceType() &&
+                pieceAlliance == otherPiece.getPieceAlliance() && isFirstPawnMove == otherPiece.isFirstPawnMove();
+    }
+    @Override
+    public int hashCode(){
+        return this.cachedHashCode;
     }
     public int getPiecePosition(){
         return this.piecePosition;
